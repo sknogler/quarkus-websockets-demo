bfc2420316bb092c1f173eb009f6268fb988d5f8
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-17 13:53:35 +0100

    add quarkus.io template code

--- src/main/java/at/htl/websockets/StartWebSocket.java
+++ src/main/java/at/htl/websockets/StartWebSocket.java
@@ -1,31 +1,52 @@
-package ilove.quark.us;
+package at.htl.websockets;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/start-websocket/{name}")
+@ServerEndpoint("/chat/{username}")
 @ApplicationScoped
 public class StartWebSocket {
 
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
     @OnOpen
-    public void onOpen(Session session, @PathParam("name") String name) {
-        System.out.println("onOpen> " + name);
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("name") String name) {
-        System.out.println("onClose> " + name);
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " left");
     }
 
     @OnError
-    public void onError(Session session, @PathParam("name") String name, Throwable throwable) {
-        System.out.println("onError> " + name + ": " + throwable);
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("name") String name) {
-        System.out.println("onMessage> " + name + ": " + message);
+    public void onMessage(String message, @PathParam("username") String username) {
+        if (message.equalsIgnoreCase("_ready_")) {
+            broadcast("User " + username + " joined");
+        } else {
+            broadcast(">> " + username + ": " + message);
+        }
     }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+
 }
